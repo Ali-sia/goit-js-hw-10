c6d0e4a25a51931c14db55890f26d2d76b13ee97
aliesia-zahorui <aliesia@ukr.net>
2022-10-03 22:28:00 +0300

    Create getGefs.js

---
+++ src/getGefs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    inputCountry: document.querySelector('#search-box'),
+    listCountry: document.querySelector('.country-list'),
+    infoCountry: document.querySelector('.country-info'),
+  };
+}
