c9335072f2c64ec045222c43049f857c8705e64b
aliesia-zahorui <aliesia@ukr.net>
2022-10-03 22:42:13 +0300

    update fetch errors

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,10 +2,18 @@ export default function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
-    if (response.status === 404) {
-      failed(response);
-    }
+    // if (response.status === 404) {
+    //   failed(response);
+    // }
 
-    return response.json();
+    // return response.json();
+
+    if (response.ok) {
+      return response.json();
+    } else if (response.status === 404) {
+      return Promise.reject('error 404');
+    } else {
+      return Promise.reject('some other error: ' + response.status);
+    }
   });
 }
